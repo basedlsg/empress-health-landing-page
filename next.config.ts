3a25d44703ae8b10187267cf4d9ef2c0b4ff735f
baseddlsg <76992468+baseddlsg@users.noreply.github.com>
2025-08-18 10:35:40 +0100

    fix: remove turbopack and update vercel.json

--- next.config.ts
+++ next.config.ts
@@ -16,13 +16,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  turbopack: {
-    rules: {
-      "*.{jsx,tsx}": {
-        loaders: [LOADER]
-      }
-    }
-  }
 };
 
 export default nextConfig;
